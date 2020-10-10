2cba36fc22a286742bf70b74c53a863935d2f9de
CintiaMurashima <cintiamurashima@gmail.com>
2020-10-10 17:32:15 -0300

    nav-client-arreglo

--- Views/header.php
+++ Views/header.php
@@ -20,7 +20,7 @@
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">  
 
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-     <script src="js/bootstrap.min.js"></script>
+     <script src="<?php echo JS_PATH ?>bootstrap.min.js"></script>
      
      <!-- <script href="<?php echo CSS_PATH ?>jquery.js"></script>
      <script href="<?php echo CSS_PATH ?>loader.js"></script> -->
