5dd15b072b22c54fae80a71d44246a0efe4964d7
CintiaMurashima <cintiamurashima@gmail.com>
2020-10-11 19:34:02 -0300

    cambios

--- Views/header.php
+++ Views/header.php
@@ -6,21 +6,20 @@
      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
      <!-- Bootstrap CSS -->
-     <link rel="stylesheet" href="<?php echo CSS_PATH ?>bootstrap.min.css">
+     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"> 
+      <!-- Bootstrap js -->
+     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
           
      <link rel="stylesheet" href="<?php echo CSS_PATH ?>myaccount.css">     
      <link rel="stylesheet" href="<?php echo CSS_PATH ?>loginFacebook.css">
      <link rel="stylesheet" href="<?php echo CSS_PATH ?>signup.css"> 
 
-     <!-- <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>login.css">
-     <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>cabecera.css">
-     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"> -->
-
      <!-- Add icon library -->
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">  
 
-     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-     <script src="<?php echo JS_PATH ?>bootstrap.min.js"></script>
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script><!--Jquery-->
      
      <!-- <script href="<?php echo CSS_PATH ?>jquery.js"></script>
      <script href="<?php echo CSS_PATH ?>loader.js"></script> -->
