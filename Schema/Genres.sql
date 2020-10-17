ff3cda86a14073a6846c8ae096525c0bd0a553b0
facundoloscos <56739064+facundoloscos@users.noreply.github.com>
2020-10-17 20:41:39 -0300

    detalles nomas

--- Schema/Genres.sql
+++ Schema/Genres.sql
@@ -1,4 +1,4 @@
-insert into genres (idGenre, `name`) values (28, 'Action'), (12, 'Adventure'), (16, 'Animation'), (35, 'Comedy'), 
-(80, 'Crime'), (99, 'Documentary'), (18, 'Drama'), (10751, 'Family'), (14, 'Fantasy'), (36, 'History'), (27, 'Horror'), 
-(10402, 'Music'), (9648, 'Mystery'), (10749, 'Romance'), (878, 'Sciense Fiction'), (10770, 'TV Movie'), (53, 'Thriller'),
-(10752, 'War'), (37, 'Western');
+insert into genres (idGenre, `name`) values (28, 'Acción'), (12, 'Aventura'), (16, 'Animación'), (35, 'Comedia'), 
+(80, 'Crimen'), (99, 'Documental'), (18, 'Drama'), (10751, 'Familiar'), (14, 'Fantasía'), (36, 'Histórica'), (27, 'Terror'), 
+(10402, 'Musical'), (9648, 'Misterio'), (10749, 'Romántica'), (878, 'Ciencia Ficción'), (10770, 'Película de TV'), (53, 'Thriller'),
+(10752, 'Bélica'), (37, 'Western');
