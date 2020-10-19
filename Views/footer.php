a1c37a2f29f5161e9c8a041303b7158cbbfee157
guimainini <guimaini@gmail.com>
2020-10-19 09:04:28 -0300

    footer

--- Views/footer.php
+++ Views/footer.php
@@ -1,5 +1,13 @@
 
-</body>
-    
-</html>
 
+<div class="wrapper row4">
+  <footer id="footer" class="clear"></footer>
+</div>
+<div class="wrapper row6">
+  <div id="copyright" class="clear" align="center"> 
+    <p >Copyright &copy; <?php echo date('Y'); ?> - All Rights Reserved - UTN Laboratorio IV</p>
+  </div>
+</div>
+
+</body>
+</html>
