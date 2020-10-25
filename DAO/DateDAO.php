5e336d49b376df53dcacd5f961647da0d0949b3c
jmRicardo <51313148+jmRicardo@users.noreply.github.com>
2020-10-25 11:33:28 -0300

    funcion de comprobacion por duracion de pelicula en funciones

--- DAO/DateDAO.php
+++ DAO/DateDAO.php
@@ -40,20 +40,40 @@
         {
             try
             {
-                $query = "SELECT FROM ".$this->tableName." WHERE  = date_format(`date`,'%Y');";
-                
+                $query = "SELECT * FROM ".$this->tableName." WHERE  date_format(`date`,'%Y-%m-%d') = :date and idMovie = :idMovie;";
+
                 $parameters["date"] = $date->getDate();
-                $parameters["idRoom"] = $date->getIdRoom();
                 $parameters["idMovie"] = $date->getIdMovie();
 
                 $this->connection = Connection::GetInstance();
 
-                $this->connection->ExecuteNonQuery($query, $parameters);
+                return $this->connection->ExecuteNonQuery($query, $parameters);
             }
             catch(Exception $ex)
             {
                 return $ex->getMessage();
             }
+        }
+
+        function CheckRuntimeWithDate(Date $date)
+        {
+            try
+            {
+                $query = "SELECT * FROM ".$this->tableName." 
+                            WHERE `date` BETWEEN ADDTIME(:date,-1500) 
+                            and date_add(:date, interval (select runtime from movies where idMovie = :idMovie)+15 minute);";
+
+                $parameters["date"] = $date->getDate();
+                $parameters["idMovie"] = $date->getIdMovie();
+
+                $this->connection = Connection::GetInstance();
+
+                return $this->connection->ExecuteNonQuery($query, $parameters);
+            }
+            catch(Exception $ex)
+            {
+                return $ex->getMessage();
+            }            
         }
     }
 
--- DAO/IDateDAO.php
+++ DAO/IDateDAO.php
@@ -7,7 +7,9 @@
 
         function AddDate(Date $date);
         function CheckDate(Date $date);
-        function CheckIfAvailable(Date $date);
+        function CheckIfAvailable(Date $date); /* Devuelve 1 si encuentra coincidencia, compara por fecha y pelicula */
+        function CheckRuntimeWithDate(Date $date);
+        
         
     }
 ?>
