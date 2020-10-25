0101c7a77f6e6e9dc6fee0b09bbcd2365a534a2c
CintiaMurashima <cintiamurashima@gmail.com>
2020-10-25 18:39:12 -0300

    trying styles select-seat

--- Views/client-select-seat.php
+++ Views/client-select-seat.php
@@ -1,3 +1,30 @@
 <?php
     require_once(VIEWS_PATH . "client-nav.php");
+    
 ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-9">
+            <?php require_once(VIEWS_PATH . "client-seat-list.php"); ?>
+        </div>
+        <div>
+            <div class="cine-box">
+                <div class="cine-header ulinepad">   
+                    <h3 class="movie-name">Pinocho</h3>
+                    <span class="dateTime">Sunday, October 25 at 4:30 pm</span>
+                    <div class="price-row ulinepad">
+                        <div id="CartSummary" class="col-xs-6">3 Tickets</div>
+                        <div id="CartSummaryValue" class="col-xs-6">$30.00</div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+                   
+   
--- views/client-seat-list.php
+++ views/client-seat-list.php
@@ -1,37 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-</head>
-<body>
-    <table>
-        <thead>
-         
-        </thead>
 
-        <form action="action.php" method="POST">
-        <tbody>
-            <?php
-                for($x = 0;$x<10;$x++){
-                    echo "<tr>";
-                    for($y = 0;$y<10;$y++)
-                    {
-                        echo "<td><button name=\"selected\" value='". $x.$y ."'> <img src='asiento.png'  onclick=\"this.src='asiento-click.png'\" </button></td>";
-                    }
-                    echo "</tr>";
-                }    
-            ?>
-        </tbody>
-        <button type="submit">ACEPTAR!</button>
-        </form>
-        
-    </table>
+<table>
+    <thead>
+    </thead>
+
+    <form action="action.php" method="POST">
+    <tbody>
+        <?php
+            for($x = 0;$x<10;$x++){
+                echo "<tr>";
+                for($y = 0;$y<10;$y++)
+                {
+                    echo "<td><button type=\"button\" name=\"selected\" value='". $x.$y ."'> <img src='". FRONT_ROOT . VIEWS_PATH ."img/asiento.png'  onclick=\"this.src='".FRONT_ROOT . VIEWS_PATH ."img/asiento-click.png'\" </button></td>";
+                }
+                echo "</tr>";
+            }    
+        ?>
+    </tbody>
+    <button type="submit">ACEPTAR!</button>
+    </form>
+</table>
                 
-
-    
-    
-</body>
-</html>
