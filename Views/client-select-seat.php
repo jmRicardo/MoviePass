bf433b592ac96506c6173395c9e1617fc707517c
CintiaMurashima <cintiamurashima@gmail.com>
2020-10-25 19:24:24 -0300

    improvements styles select-seat

--- Views/client-select-seat.php
+++ Views/client-select-seat.php
@@ -5,20 +5,32 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-lg-9">
+        <div class="col-lg-7">
             <?php require_once(VIEWS_PATH . "client-seat-list.php"); ?>
         </div>
-        <div>
+        <div class="col-lg-5">
             <div class="cine-box">
-                <div class="cine-header ulinepad">   
-                    <h3 class="movie-name">Pinocho</h3>
-                    <span class="dateTime">Sunday, October 25 at 4:30 pm</span>
-                    <div class="price-row ulinepad">
-                        <div id="CartSummary" class="col-xs-6">3 Tickets</div>
-                        <div id="CartSummaryValue" class="col-xs-6">$30.00</div>
-                    </div>
-
-                </div>
+                    <div class="cine-header">   
+                        <h3 class="movie-name">Pinocho</h3>
+                        <span class="dateTime">Sunday, October 25 at 4:30 pm</span>
+                        <div>
+                            <span>Cinema Center</span>
+                            <span>Santiago del estero 3047</span>
+                            <span>Sala 2</span>
+                        </div>
+                        <div class="price-row">
+                            <div>
+                                <div id="CartSummary" class="col-xs-6">3 Tickets</div>
+                                <div id="CartSummaryValue" class="col-xs-6">$30.00</div>
+                            </div> 
+                            <h2 id="CartTotalAmount" class="text-right noline top">
+                                Total &nbsp; <span id="CartTotalAmountValue">$37.71</span>
+                            </h2>
+                        </div>
+                        <a id="movie-id">
+                            <button type="submit" class="btn btn-success" id="movie-reservation">Confirmar Reserva</button>
+                        </a>
+                 </div>
             </div>
         </div>
     </div>
