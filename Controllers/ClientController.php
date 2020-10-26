50459750fd6e02d7748214574f49f3d2d9335426
CintiaMurashima <cintiamurashima@gmail.com>
2020-10-25 23:22:33 -0300

    tabs working

--- Controllers/ClientController.php
+++ Controllers/ClientController.php
@@ -1,19 +1,22 @@
 <?php
     namespace Controllers;
     use DAO\MovieDAO;
+    use DAO\DateDAO;
 
     class ClientController 
     {
         private $movieDao;
+        private $dateDao;
 
         function __construct() 
         {
-            $this->movieDao= new MovieDAO();
+            $this->movieDao = new MovieDAO();
+            $this->dateDao = new DateDAO();
         }
         
         function Home($message = "") 
         {
-            $movies=$this->movieDao->GetAll();
+            $movies = $this->movieDao->GetAll();
             require_once(VIEWS_PATH."client-home.php");
         }
 
@@ -25,26 +28,38 @@
         function Select() 
         {
             $idGenre = 0;
-            $genres=$this->movieDao->GetActiveGenres();
-            $movies=$this->movieDao->GetAll();
+            $genres = $this->movieDao->GetActiveGenres();
+            $movies = $this->movieDao->GetAll();
             require_once(VIEWS_PATH."client-select-movie.php");
         }
 
         function Filter($idGenre)
         {
-            $genres=$this->movieDao->GetActiveGenres();
-            $movies=$this->movieDao->GetMoviesByGenre($idGenre);
+            $genres = $this->movieDao->GetActiveGenres();
+            $movies = $this->movieDao->GetMoviesByGenre($idGenre);
             require_once(VIEWS_PATH."client-select-movie.php");
         }
 
         function SelectDate($idMovie)
         {
-            $movie=$this->movieDao->GetMovieByID($idMovie);
+            $movie = $this->movieDao->GetMovieByID($idMovie);
+            $dates = $this->dateDao->GetDatesFromWeek($idMovie);
+            function getDatesByDay($day, $dates) {
+                $datesOfDay = array();
+                $currentDay = new \DateTime($day);
+                foreach($dates as $date) {
+                    $dateToCompare = new \DateTime($date->getDate());
+                    $interval = $currentDay->diff($dateToCompare);
+                    if ($interval->days === 0) {
+                        array_push($datesOfDay, $date);
+                    }
+                }
+            }
             require_once(VIEWS_PATH."client-select-date.php");
         }
         function listCarusel()
         {
-            $movies=$this->movieDao->GetAll();
+            $movies = $this->movieDao->GetAll();
             require_once(VIEWS_PATH."client-list-carusel.php");
         }
 
--- Views/client-select-date.php
+++ Views/client-select-date.php
@@ -20,21 +20,21 @@
         <div class="col-lg-8">
             <h2><?php echo $movie->getTitle()?></h2>
             <p><b>Géneros: </b><span><?php echo listGenres($movie->getIdMovie())?></span></p>
-            <ul class="nav nav-tabs day-selector">
+            <ul class="nav nav-tabs day-selector" id="myTab" role="tablist">
                 <?php
                     setlocale(LC_TIME,"spanish");
                     $day = new DateTime();
                     define("CHARSET", "iso-8859-1");
                 ?>
-                <li class="nav-item day-item">
-                    <a class="nav-link day-link selected-day active " href="#"><?php echo utf8_encode(strftime("%A %#d", $day->getTimestamp())); ?></a>
+                <li class="nav-item day-item" role="presentation">
+                    <a class="nav-link day-link selected-day active" id="date-26-tab" data-toggle="tab" href="#date-26" role="tab" aria-controls="date-26" aria-selected="true"><?php echo utf8_encode(strftime("%A %#d", $day->getTimestamp())); ?></a>
                 </li>
                 <?php
-                    for ($i=0; $i < 6; $i++) {
+                    for ($i=0; $i < 1; $i++) {
                         $day->modify("+1 day");
                         ?>
-                        <li class="nav-item day-item">
-                            <a class="nav-link day-link" href="#"><?php echo utf8_encode(strftime("%A %#d", $day->getTimestamp())); ?></a>
+                        <li class="nav-item day-item" role="presentation">
+                            <a class="nav-link day-link" id="date-27-tab" data-toggle="tab" href="#date-27" role="tab" aria-controls="date-27" aria-selected="true"><?php echo utf8_encode(strftime("%A %#d", $day->getTimestamp())); ?></a>
                         </li>
                         <?php
                     }
@@ -43,34 +43,23 @@
                     <a class="nav-link day-link" href="#">Ver más</a>
                 </li>
             </ul>
-            <div class="row">  
-                <div class="col-lg-6">
-                    <div class="cine-box">
-                        <div class="cine-header">
-                            <h3 class="cine-name">Cines del paseo</h3>
-                            <span>Santiago del estero 2020</span>
-                        </div>
-                        <div class="cine-times">
-                            <button type="button" class="btn btn-warning cine-time">05:30 pm</button>
-                            <button type="button" class="btn btn-warning cine-time">06:30 pm</button>
-                            <button type="button" class="btn btn-warning cine-time">06:30 pm</button>
-                            <button type="button" class="btn btn-warning cine-time">10:30 pm</button>
-                            <button type="button" class="btn btn-warning cine-time">06:30 pm</button>
-                            <button type="button" class="btn btn-warning cine-time">06:30 pm</button>
-                            <button type="button" class="btn btn-warning cine-time">06:30 pm</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="cine-box">
-                        <div class="cine-header">
-                            <h3 class="cine-name">Pompas</h3>
-                            <span>Rivadavia 20527</span>
-                        </div>
-                        <div class="cine-times">
-                            <button type="button" class="btn btn-warning cine-time">05:30 pm</button>
-                            <button type="button" class="btn btn-warning cine-time">06:30 pm</button>
-                            <button type="button" class="btn btn-warning cine-time">06:30 pm</button>
+            <div class="row tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="date-26" role="tab-panel" aria-labelledby="date-26-tab">
+                    <div class="col-lg-6">
+                        <div class="cine-box">
+                            <div class="cine-header">
+                                <h3 class="cine-name">Cines del paseo</h3>
+                                <span>Santiago del estero 2020</span>
+                            </div>
+                            <div class="cine-times">
+                                <button type="button" class="btn btn-warning cine-time">05:30 pm</button>
+                                <button type="button" class="btn btn-warning cine-time">06:30 pm</button>
+                                <button type="button" class="btn btn-warning cine-time">06:30 pm</button>
+                                <button type="button" class="btn btn-warning cine-time">10:30 pm</button>
+                                <button type="button" class="btn btn-warning cine-time">06:30 pm</button>
+                                <button type="button" class="btn btn-warning cine-time">06:30 pm</button>
+                                <button type="button" class="btn btn-warning cine-time">06:30 pm</button>
+                            </div>
                         </div>
                     </div>
                 </div>
