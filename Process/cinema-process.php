ccd314e4214937d1a8ace8822e6a7bcd189946ea
facundoloscos <56739064+facundoloscos@users.noreply.github.com>
2020-10-28 18:04:01 -0300

    mas validaciones

--- Process/cinema-process.php
+++ Process/cinema-process.php
@@ -4,15 +4,18 @@
 
     $cinemaDAO = new CinemaDAO();
 
-    if('' == $cinema->getName() || empty($cinema->getName())) {
+    if('' == $cinema->getName() || empty($cinema->getName()) || strlen($cinema->getName()) <= 3 || strlen($cinema->getName()) >= 25) {
 
         $_SESSION['message'] = 'Nombre de cine invalido';
 
-    } elseif(''  == $cinema->getAddress() || empty($cinema->getAddress())) {
+    } elseif(''  == $cinema->getAddress() || empty($cinema->getAddress()) || strlen($cinema->getAddress()) <= 3 || strlen($cinema->getAddress()) >= 25) {
 
         $_SESSION['message'] = 'Direccion invalida';
 
     } else {
+
+        /* $cineTrimed = trim($cinema->getName());
+        $cinema->setName($cineTrimed);  */
        
         $error = $this->cinemaDAO->Add($cinema);
 
@@ -24,10 +27,7 @@
 
     }
 
-    
 
     header("Location:".FRONT_ROOT."Admin/ShowCinemaList");
-
-
 
 ?>
--- Process/room-process.php
+++ Process/room-process.php
@@ -6,12 +6,14 @@
 
     
 
-    if('' == $room->getName() || empty($room->getName())) {
+    if('' == $room->getName() || empty($room->getName()) || strlen($room->getName()) <= 2 || strlen($room->getName()) >= 25) {
         
         $_SESSION['message'] = 'Nombre de sala invalido';
+
     } elseif(!is_numeric($room->getPrice()) || $room->getPrice() <= 0) {
 
         $_SESSION['message'] = 'Valor de entrada invalido';
+
     }else{
 
         $error = $this->roomDAO->Add($room);
