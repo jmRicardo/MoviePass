7730c62e3bc14f697a8b3d39a9f25de644edd281
guimainini <guimaini@gmail.com>
2020-11-03 13:08:06 -0300

    estadisticas

--- Views/admin/admin-ticket-sold.php
+++ Views/admin/admin-ticket-sold.php
@@ -2,7 +2,115 @@
 require_once('admin-nav.php');
 ?>
 
+
 <main class="py-5">
+
+<section id="listado" class="mb-5">
+    <div class="container">
+        <h2 class="mb-4">Listado de Cines</h2>
+            <table class="table bg-light-alpha">
+                <thead>
+                    <th>Nombre</th>
+                    <th>Direccion</th>
+                    <th></th>
+                    <th>Recaudacion</th>
+
+                </thead>
+                    <tbody>
+                        <form action="<?php echo FRONT_ROOT ?>Admin/ShowRoomByTicketSold" method="GET" id="from">
+                            <?php foreach ($cinemaList as $cinema) { ?>
+                                <tr>
+                                    <td><?php echo $cinema->getName() ?></td>
+                                    <td><?php echo $cinema->getAddress() ?></td>
+                                    <td>
+                                    <button type="submit" name="id" class="btn btn-success" value="<?php echo $cinema->getId() ?>">
+                                        Total Sala
+                                    </button>
+                                    </td>
+                                    <!-- <td>
+                                        <button name="id" value="<?php echo $cinema->getId() ?>" class="btn btn-warning" onclick="
+                                            var frm = document.getElementById('from') || null;
+                                            if(frm) {
+                                                frm.action = '<?php echo FRONT_ROOT ?>'.concat('Admin/Update');     
+                                            }">
+                                            <i class="fa fa-edit"></i>
+                                            Modificar
+                                        </button>
+                                    </td>
+                                    <td>
+                                        <button name="id" value="<?php echo $cinema->getId() ?>" class="btn btn-success" onclick="
+                                            var frm = document.getElementById('from') || null;
+                                                if(frm) {
+                                                frm.action = '<?php echo FRONT_ROOT ?>'.concat('Admin/ShowAddRoom');     
+                                                }">
+                                        <i class="fa fa-edit"></i>
+                                                Administrar Salas
+                                        </button>
+                                    </td> -->
+                                    <td>
+                                    $15000
+                                    </td> 
+                                </tr>
+                                     
+                            <?php }?>
+                                                
+                        </form>
+                    </tbody>
+            </table>
+        </div>
+    </section>
+
+</main>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <main class="py-5">
     <div class="col-lg-5">
         <div class="cine-box">
             <div class="cine-header">
@@ -51,4 +159,4 @@ require_once('admin-nav.php');
             </div>
         </div>
     </div>
-</main>
+</main> -->
