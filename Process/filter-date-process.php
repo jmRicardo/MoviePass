6b22a44b4c3f592e02997a111fb46f5a32b22a1a
Guillermo Mainini <guimainini@gmail.com>
2020-11-07 12:49:02 -0300

    validacion de fecha estadistica

--- Process/filter-date-process.php
+++ Process/filter-date-process.php
@@ -1,5 +1,22 @@
 <?php
 
+// var_dump($start);
+// var_dump($end);
+
+// $day = new DateTime();
+// $cDayMin = $day->format('Y-m-d');
+
+// $mDayMax = $day3month->format('Y-m-d');
+
+if($start > $end) {
+        
+    $_SESSION['message'] = "No te hagas el vivo cara de pija y pone bien la fecha";
+    // require_once(ADMIN_PATH."admin-filter-date.php");
+    $final = "0";
+    header("Location:".FRONT_ROOT."admin-filter-date.php");
+    $result = null;
+}
+
     
 if(isset($result)){
                                 
@@ -11,22 +28,22 @@ if(isset($result)){
 
     if ($result)
     {
-    if ($idMovie == 'TODES' && $cinema == 'TODES')
-    {
-        $final = "De " . $start . " hasta " . $end . "<br>". "El total recaudado en ese período de tiempo es: $" .$total;       
-    }
-    elseif ($cinema == 'TODES')
-    {
-        $final = "De " . $start . " hasta " . $end . "<br>" . "La pelicula " .$result[0]['PELICULA']. " recaudo un total de: $" . $total;                   
-    }
-    elseif ($idMovie == 'TODES')
-    {
-        $final = "De " . $start . " hasta " . $end . "<br>" . "El cine " .$result[0]['CINE'] . " recaudo un total de $". $total;
-    }
-    else
-    {
+        if ($idMovie == 'TODES' && $cinema == 'TODES')
+        {
+            $final = "De " . $start . " hasta " . $end . "<br>". "El total recaudado en ese período de tiempo es: $" .$total;       
+        }
+        elseif ($cinema == 'TODES')
+        {
+            $final = "De " . $start . " hasta " . $end . "<br>" . "La pelicula " .$result[0]['PELICULA']. " recaudo un total de: $" . $total;                   
+        }
+        elseif ($idMovie == 'TODES')
+        {
+            $final = "De " . $start . " hasta " . $end . "<br>" . "El cine " .$result[0]['CINE'] . " recaudo un total de $". $total;
+        }
+        else
+        {
         $final = "De " . $start . " hasta " . $end . "<br>" . "La pelicula " .$result[0]['PELICULA']. " recaudo un total de: $" . $total . " en el cine ". $result[0]['CINE'];                    
-    }
+        }
     }else {
         $final = "Pelicula sin ser vista";
     }
